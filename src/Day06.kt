313653ef01af833d3d3d36777b4afb57835296d4
Elliott Wallace <elliott.d.wallace@gmail.com>
2024-12-14 07:52:02 +0100

    Day 6 part two

--- src/Day06.kt
+++ src/Day06.kt
@@ -12,9 +12,7 @@ fun main() {
         }
     }
 
-    fun part1(input : List<String>): Int {
-        val grid = parseCharacterGrid(input)
-        //val gridCopy = grid.map { it.clone() }.toTypedArray()
+    fun simulateGuardPath(grid: Array<CharArray>): Pair<MutableSet<Guard>, Boolean> {
         val cols = grid[0].size
         val rows = grid.size
 
@@ -36,7 +34,8 @@ fun main() {
         }
 
         val guard = Guard(initialPosition, initialDirection)
-        val visited = mutableSetOf(initialPosition)
+        var isCycle = false
+        val visited = mutableSetOf(guard.copy())
 
         while (true) {
             val (posX, posY) = guard.position
@@ -44,35 +43,61 @@ fun main() {
             var nextPosition = posX + deltaX to posY + deltaY
 
             if (nextPosition.first >= cols || nextPosition.first < 0
-                ||nextPosition.second >= rows || nextPosition.second < 0) break
+                || nextPosition.second >= rows || nextPosition.second < 0
+            ) break
 
-            if (grid[nextPosition.second][nextPosition.first] == '#') {
+            while (grid[nextPosition.second][nextPosition.first] == '#') {
                 turnRight(guard)
                 nextPosition = posX + guard.direction.dir.first to posY + guard.direction.dir.second
             }
 
             if (nextPosition.first >= cols || nextPosition.first < 0
-                ||nextPosition.second >= rows || nextPosition.second < 0) break
+                || nextPosition.second >= rows || nextPosition.second < 0
+            ) break
 
             guard.position = nextPosition
+            val guardState = guard.copy()
+
             //gridCopy[nextPosition.second][nextPosition.first] = 'X'
-            visited.add(nextPosition)
+            isCycle = !visited.add(guardState)
+            if (isCycle) break
         }
+        return visited to isCycle
+    }
 
+    fun part1(input : List<String>): Int {
+        val grid = parseCharacterGrid(input)
+        //val gridCopy = grid.map { it.clone() }.toTypedArray()
+        val guardStates = simulateGuardPath(grid)
 
-        return visited.size
+        return guardStates.first.map { it.position }.toSet().size
     }
 
     fun part2(input : List<String>): Int {
+        val initialGrid = parseCharacterGrid(input)
+        val initialGuardPath = simulateGuardPath(initialGrid).first
 
-        return 0
+        var cycleCount = 0
+        initialGuardPath.map { it.position }.toSet().forEach { position ->
+            val posX = position.first
+            val posY = position.second
+            val modifiedGrid = initialGrid.map { it.clone() }.toTypedArray()
+            if (modifiedGrid[posY][posX] == '.') {
+                modifiedGrid[posY][posX] = '#'
+            }
+            val (_, isCycle) = simulateGuardPath(modifiedGrid)
+            if (isCycle) {
+                cycleCount++
+            }
+        }
+        return cycleCount
     }
 
     val testInput = readInput("Day06_test")
     check(part1(testInput) == 41)
-    //check(part2(testInput) == 123)
+    check(part2(testInput) == 6)
 
     val input = readInput("Day06")
     part1(input).println()
-    //part2(input).println()
+    part2(input).println()
 }
